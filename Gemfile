9ca31d953982ff1c1821cbb7ea7aa0d8b9eee94a
Daniel Nordstrom <d@nintera.com>
2012-01-24 04:04:19 +0100

    Styling and layout changes.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'http://rubygems.org'
 
 gem 'sinatra'
 gem 'thin'
+gem 'slim'
+gem 'sass'
--- application.rb
+++ application.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+get '/stylesheets/application.css' do
+  sass :'../stylesheets/application'
+end
+
 get '/' do
   slim :home
 end
