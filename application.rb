37f61dc6444e888f74703c4ef0d4a0a664c599e4
Daniel Nordstrom <d@nintera.com>
2012-01-24 14:31:01 +0100

    Adding page views.

--- application.rb
+++ application.rb
@@ -5,5 +5,29 @@ get '/stylesheets/application.css' do
 end
 
 get '/' do
-  slim :home
+  slim :about
+end
+
+get '/contributors' do
+  slim :contributors
+end
+
+get '/credit' do
+  slim :credit
+end
+
+get '/suggestions' do
+  slim :suggestions
+end
+
+get '/award' do
+  slim :award
+end
+
+get '/donate' do
+  slim :donate
+end
+
+get '/donors' do
+  slim :donors
 end
